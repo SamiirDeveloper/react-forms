e9740778f5196f647107ca8a61184de06999881d
Samir <samirstown@hotmail.com>
2025-07-29 22:55:21 -0700

    Added FindDeletePost and UpdatePost components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import CreatePost from "./components/CreatePost"; 
+import FindDeletePost from "./components/FindDeletePost";
+import UpdatePost from "./components/UpdatePost";
 import './App.css'
 
 function App() {
   return (
     <>
     <createPost/>
+    <FindDeletePost/>
+    <UpdatePost/>
     </>
   )
 }
